af59ee451bb9bf46b148e431a492958a141674c6
Earl <shepherdmoahloli122@gmail.com>
2023-06-06 10:56:57 +0200

    just need the fix tailwind to respond

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let counter = 15;
+    let x = Math.round();
+    let y = 15;
+    let z;
+
+    $: z = x + y;
+    function increase() {
+        counter++;
+    }
+
+    const decrese = () => {
+        counter--;
+    };
+</script>
+
+<svelte>
+    <title>Slender never gets tired</title>
+</svelte>
+<h1 class="bg-blue-600">Kalcata</h1>
+
+<input type="number" bind:value={x} />
++
+<input type="number" bind:value={y} />
+=
+<input type="number" bind:value={z} readonly />
+
+<h1>Hello, Hood!</h1>
+<p>And the count is: {counter}</p>
+<button on:click={increase} class="bg-blue-200">+</button>
+<button on:click={decrese}>-</button>
+
+<style>
+    h1 {
+        color: #909090;
+    }
+</style>
